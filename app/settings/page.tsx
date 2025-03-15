abf634c58dd0b8432fdf698941fcd577b13a2aa5
Josh XT <josh@devxt.com>
2025-03-15 16:42:32 -0400

    remove more unused code

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,11 +1,9 @@
 import { Providers } from '@/components/interactive/Settings/providers';
-import AgentPanel from '@/components/interactive/Settings/agent/AgentPanel';
 import { SidebarPage } from '@/components/idiot/appwrapper/SidebarPage';
 
 export default function ProvidersPage() {
   return (
     <SidebarPage title='Settings'>
-      <AgentPanel />
       <Providers />
     </SidebarPage>
   );
