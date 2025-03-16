b2e2f94b6670e03082fe79f68bd4960c5b77dc40
Josh XT <josh@devxt.com>
2025-03-16 07:40:36 -0400

    more clean up

--- app/team/layout.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { ReactNode } from 'react';
-import { SidebarInset } from '@/components/ui/sidebar';
-
-export default function TeamLayout({ children }: { children: ReactNode }): ReactNode {
-  return <SidebarInset>{children}</SidebarInset>;
-}
--- app/team/page.tsx
+++ app/team/page.tsx
@@ -1,14 +1,17 @@
-import { SidebarPage } from '@/components/idiot/appwrapper/SidebarPage';
 import Team from '@/components/idiot/auth/management/Team';
 import TeamUsers from '@/components/idiot/auth/management/TeamUsers';
+import { SidebarInset } from '@/components/ui/sidebar';
+import { SidebarPage } from '@/components/idiot/appwrapper/SidebarPage';
 
 export default function TeamPage() {
   return (
-    <SidebarPage title='Team Management'>
-      <div className='overflow-x-auto px-4'>
-        <Team />
-        <TeamUsers />
-      </div>
-    </SidebarPage>
+    <SidebarInset>
+      <SidebarPage title='Team Management'>
+        <div className='overflow-x-auto px-4'>
+          <Team />
+          <TeamUsers />
+        </div>
+      </SidebarPage>
+    </SidebarInset>
   );
 }
