556acfe1727dbaed3bc0331961e6cbcffd381843
Josh XT <josh@devxt.com>
2025-03-16 07:15:57 -0400

    simplify sidebar

--- components/idiot/appwrapper/SidebarPage.tsx
+++ components/idiot/appwrapper/SidebarPage.tsx
@@ -1,5 +1,7 @@
 import { ReactNode } from 'react';
-import { SidebarHeader, SidebarHeaderTitle, SidebarMain } from './SidebarHeader';
+import { Separator } from '@/components/ui/separator';
+import { SidebarTrigger } from '@/components/ui/sidebar';
+import { cn } from '@/lib/utils';
 
 interface SidebarPageProps {
   title: string;
@@ -10,10 +12,22 @@ interface SidebarPageProps {
 export function SidebarPage({ title, children, className }: SidebarPageProps) {
   return (
     <>
-      <SidebarHeader>
-        <SidebarHeaderTitle>{title}</SidebarHeaderTitle>
-      </SidebarHeader>
-      <SidebarMain className={className}>{children}</SidebarMain>
+      <header
+        className='flex shrink-0 items-center gap-2 px-6 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 w-full sticky top-0 z-20 bg-background/80 backdrop-blur supports-[backdrop-filter]:bg-background/60'
+        style={{ paddingTop: 'env(safe-area-inset-top)', height: 'calc(3rem + env(safe-area-inset-top))' }}
+      >
+        <div className='flex items-center h-full md:hidden'>
+          <SidebarTrigger className='size-10' />
+          <Separator orientation='vertical' className='h-4' />
+        </div>
+        <span className='mx-auto text-muted-foreground'>{title}</span>
+      </header>
+      <main
+        className={cn('flex flex-col flex-1 gap-6 px-6 py-4', className)}
+        style={{ paddingBottom: 'env(safe-area-inset-bottom)' }}
+      >
+        {children}
+      </main>
     </>
   );
 }
