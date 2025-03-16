553ded0bfe16ff89b46f445f64d555e1e73ed7c7
Josh XT <josh@devxt.com>
2025-03-16 09:58:41 -0400

    add panel back

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,9 +1,11 @@
 import { Providers } from '@/components/agent/providers';
+import AgentPanel from '@/components/agent/AgentPanel';
 import { SidebarPage } from '@/components/layout/SidebarPage';
 
 export default function ProvidersPage() {
   return (
     <SidebarPage title='Settings'>
+      <AgentPanel />
       <Providers />
     </SidebarPage>
   );
