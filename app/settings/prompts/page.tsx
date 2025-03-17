fda5e02f6690395d6509ecff0e97262e59c7b22c
Josh XT <josh@devxt.com>
2025-03-17 13:17:21 -0400

    Add titlebar to prompts page

--- app/settings/prompts/page.tsx
+++ app/settings/prompts/page.tsx
@@ -1,29 +1,19 @@
 'use client';
 
 import { useState } from 'react';
-import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-import { useInteractiveConfig } from '@/components/idiot/interactive/InteractiveConfigContext';
 import PromptPanel from '@/components/prompt/PromptPanel';
 import NewPromptDialog from '@/components/prompt/PromptDialog';
+import { SidebarPage } from '@/components/layout/SidebarPage';
 
 export default function PromptPage() {
   const [showCreateDialog, setShowCreateDialog] = useState(false);
-  const context = useInteractiveConfig();
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
-  const router = useRouter();
 
   return (
-    <div className='container mx-auto p-6 space-y-6'>
-      <PromptPanel
-        showCreateDialog={showCreateDialog}
-        setShowCreateDialog={setShowCreateDialog}
-        context={context}
-        searchParams={searchParams}
-        pathname={pathname}
-        router={router}
-      />
-      <NewPromptDialog open={showCreateDialog} setOpen={setShowCreateDialog} />
-    </div>
+    <SidebarPage title='Prompt Management'>
+      <div className='container mx-auto p-6 space-y-6'>
+        <PromptPanel />
+        <NewPromptDialog open={showCreateDialog} setOpen={setShowCreateDialog} />
+      </div>
+    </SidebarPage>
   );
 }
