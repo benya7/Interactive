ea7f24a2f251a36b0fcaf38193744a3f999f77a1
Josh XT <josh@devxt.com>
2025-03-22 14:32:06 -0400

    fix styling

--- app/user/subscribe/page.tsx
+++ app/user/subscribe/page.tsx
@@ -1,6 +1,11 @@
 'use client';
 import Subscribe from '@/components/auth/Subscribe';
+import { SidebarInset } from '@/components/ui/sidebar';
 
 export default function Page({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined } }) {
-  return <Subscribe searchParams={searchParams} />;
+  return (
+    <SidebarInset>
+      <Subscribe searchParams={searchParams} />
+    </SidebarInset>
+  );
 }
